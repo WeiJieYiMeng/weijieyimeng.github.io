5c6de3ffa7e8755fc9367eb5bf21daf3329f3d24
WeiJieYiMeng <3287195988@qq.com>
2025-11-04 20:56:50 +0800

    Add files via upload

---
+++ styleFit.js
@@ -0,0 +1,48 @@
+window.addEventListener("message",(event)=>{
+  if(event.origin!=location.origin)throw new Error("通信非同源");
+  if(event.data.styleFit)eval("styleFit."+event.data.styleFit+"()");
+})
+var styleFit={
+  fontSize : (h=1)=>{
+    let customFontSize = JSON.parse( localStorage.getItem("fontSize") || 16);
+    document.documentElement.style.fontSize=customFontSize+"px";
+    if(h){window.top.hint("字号："+customFontSize+"px");}
+  },
+  lineHeight : (h=1)=>{
+    let customLineHeight = JSON.parse( localStorage.getItem("lineHeight") || 1.3);
+    document.documentElement.style.lineHeight=customLineHeight;
+    if(h){window.top.hint("行距："+customLineHeight+"倍行距");}
+  },
+  paddingMode : (h=1)=>{
+    let customPaddingMode = Number(JSON.parse(localStorage.getItem("paddingMode")));
+    switch(customPaddingMode){
+      case 0:document.documentElement.style.setProperty('--content-max-width', "auto");
+      case 3:document.documentElement.style.setProperty("--content-padding","0.618rem 0");
+      case 2:document.documentElement.style.setProperty("--reader-padding","0");break;
+      case 1:if(h){
+        document.documentElement.style.setProperty("--reader-padding","0rem 2rem");
+        document.documentElement.style.setProperty("--content-padding","0.618rem 1.618rem");
+        document.documentElement.style.setProperty("--content-max-width","800px");
+      };break;
+    }
+    if(h){window.top.hint("边框模式："+window.top.uiPaddingMode(customPaddingMode))}
+  },
+  chapterAlways : (h=1)=>{
+    let customChapterAlways = JSON.parse(localStorage.getItem("chapterAlways")) === true;
+    try{
+      document.querySelector(".chapter-bar").classList.toggle('chapter-always', customChapterAlways);
+      if(h){window.top.hint(customChapterAlways?"强制显示切换键\n再次点击按钮以取消":"取消强制显示切换键"); }
+    }catch{window.top.hint("StyleFit:\nNo chapter-bar")}
+  },
+  darkMode : (h=1)=>{
+    let customDarkMode = JSON.parse(localStorage.getItem("darkMode")) === true;
+    document.body.classList.toggle('dark-mode', customDarkMode);
+    if(h){window.top.hint(customDarkMode?"深色模式":"浅色模式"); }
+  },
+  all : ()=>{
+    Object.keys(styleFit).forEach(key => {
+      if(key=="all")return 0;
+      eval("styleFit."+key+"(0)")
+    });
+  }
+}
